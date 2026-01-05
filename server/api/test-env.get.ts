407e7ae515b53df19d495e257a81f10efa354efa
Yanisse FERHAOUI <yanisseferhaoui@gmail.com>
2026-01-05 23:23:27 +0100

    delete env-test

--- server/api/test-env.get.ts
+++
@@ -1,9 +0,0 @@
-export default defineEventHandler((event) => {
-  const config = useRuntimeConfig()
-
-  return {
-    resendApiKey: config.resendApiKey || null,
-    adminEmail: config.adminEmail || null,
-    supabaseUrl: config.supabaseUrl || null
-  }
-})
